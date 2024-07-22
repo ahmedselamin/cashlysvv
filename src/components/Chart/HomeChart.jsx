dba0250a4d04ee5cc2f70cc2ae2715a506f38f63
eddyiiz <eddyxsayf@gmail.com>
2024-07-22 19:47:20 +0200

    abstracting charts to a single folder

---
+++ src/components/Chart/HomeChart.jsx
@@ -0,0 +1,62 @@
+// DonutChart.js
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+import "./Chart.css";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const DonutChart = () => {
+  const data = {
+    labels: [
+      "Rent",
+      "Groceries",
+      "Utilities",
+      "Entertainment",
+      "Miscellaneous",
+    ],
+    datasets: [
+      {
+        label: "Expenses",
+        data: [500, 200, 100, 150, 50],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      tooltip: {
+        enabled: true,
+      },
+    },
+  };
+
+  return (
+    <div style={{ width: "70%", margin: "1em auto" }}>
+      <h2>Expense Distribution</h2>
+      <Doughnut data={data} options={options} />
+    </div>
+  );
+};
+
+export default DonutChart;
