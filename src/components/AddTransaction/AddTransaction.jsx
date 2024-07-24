dda2541deda8188fde6966d97669c7fd9995cc44
eddyiiz <eddyxsayf@gmail.com>
2024-07-24 23:22:39 +0200

    transaction component

--- src/components/AddTransaction/AddTransaction.jsx
+++ src/components/AddTransaction/AddTransaction.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const AddTransaction = () => {
-  return <div>AddTransaction</div>;
+  return (
+    <div className="transaction-container">
+      <form>
+        <div className="form-group">
+          <label>Amount</label>
+          <input type="number" required />
+        </div>
+        <div className="form-group">
+          <label>title</label>
+          <input type="text" required />
+        </div>
+        <div className="form-group">
+          <label>Date</label>
+          <input type="date" required />
+        </div>
+        <div className="form-group">
+          <label>category</label>
+          <select>
+            <option>Expense</option>
+            <option>Income</option>
+          </select>
+        </div>
+        <button>Save Trancaction</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddTransaction;
