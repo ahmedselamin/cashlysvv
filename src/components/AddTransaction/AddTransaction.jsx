44edce73cee95d3efb0b968487377b03b9397b87
eddyiiz <eddyxsayf@gmail.com>
2024-07-24 23:41:43 +0200

    form changes and styling

--- src/components/AddTransaction/AddTransaction.jsx
+++ src/components/AddTransaction/AddTransaction.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 
+import "./AddTransaction.css";
+
 const AddTransaction = () => {
   return (
-    <div className="transaction-container">
-      <form>
-        <div className="form-group">
-          <label>Amount</label>
-          <input type="number" required />
-        </div>
-        <div className="form-group">
-          <label>title</label>
-          <input type="text" required />
-        </div>
-        <div className="form-group">
-          <label>Date</label>
-          <input type="date" required />
-        </div>
-        <div className="form-group">
-          <label>category</label>
-          <select>
-            <option>Expense</option>
-            <option>Income</option>
-          </select>
-        </div>
-        <button>Save Trancaction</button>
-      </form>
+    <div className="add-transaction">
+      <div className="transaction-container">
+        <h2>Add Transaction</h2>
+        <form>
+          <div className="form-group">
+            <label>Title</label>
+            <input type="text" required />
+          </div>
+          <div className="form-group">
+            <label>Amount</label>
+            <input type="number" required />
+          </div>
+          <div className="form-group">
+            <label>Category</label>
+            <select>
+              <option>Expense</option>
+              <option>Income</option>
+            </select>
+          </div>
+          <div className="form-group-date">
+            <label>Date</label>
+            <input type="date" required />
+          </div>
+          <button>Save Trancaction</button>
+        </form>
+      </div>
     </div>
   );
 };
