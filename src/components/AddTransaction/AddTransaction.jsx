ef8be2f75b56186c62c518dea36c1a4d3b411c2d
eddyiiz <eddyxsayf@gmail.com>
2024-07-25 21:15:41 +0200

    back button and more styling

--- src/components/AddTransaction/AddTransaction.jsx
+++ src/components/AddTransaction/AddTransaction.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./AddTransaction.css";
 
 const AddTransaction = () => {
+  const navigate = useNavigate();
   return (
     <div className="add-transaction">
       <div className="transaction-container">
+        <button className="back-btn" onClick={() => navigate("/home")}>
+          <i class="fa fa-arrow-left" aria-hidden="true"></i>
+        </button>
         <h2>Add Transaction</h2>
+
         <form>
           <div className="form-group">
             <label>Title</label>
@@ -27,6 +33,7 @@ const AddTransaction = () => {
             <label>Date</label>
             <input type="date" required />
           </div>
+
           <button className="login-btn">Save Trancaction</button>
         </form>
       </div>
