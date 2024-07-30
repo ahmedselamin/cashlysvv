8f262ae7e0ed7476e40a86bb689913070b81c8cd
eddyiiz <eddyxsayf@gmail.com>
2024-07-30 10:04:19 +0200

    settings page

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import "./Settings.css";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div className="settings-container">
+      <h1>Account Settings</h1>
+      <form className="form-group">
+        <label>
+          Change Password
+          <input type="password" placeholder="new password" />
+        </label>
+        <button className="change-pw-btn">Change</button>
+      </form>
+      <button className="delete-acc-btn"> Delete Account</button>
+    </div>
+  );
 };
 
 export default Settings;
